c68d79529018f72738542c96a70125a454fd83d0
AlmeidaMurillo <almeidamurillo196@gmail.com>
2025-10-19 17:00:11 -0300

    att backg

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const cron = require("node-cron");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
